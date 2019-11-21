ecb3651f3c063d1f0b2febe71aa5a8b8069a0ba2
Dangkhoa Nguyen <33072520+DangkhoaNguyen@users.noreply.github.com>
2019-11-20 22:36:01 -0500

    Created CargoPivot

---
+++ src/main/java/frc/robot/subsystems/cargo/CargoPivot.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems.cargo;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.enums.CargoPivotState;
+
+public class CargoPivot extends Subsystem {
+    private static CargoPivot instance;
+    private static DoubleSolenoid doubleSolenoid;
+    private static CargoPivotState pivotstate;
+    public CargoPivot() {
+        doubleSolenoid = new DoubleSolenoid(0, 1);
+    }
+
+    public void setDoubleSolenoidState(CargoPivotState state) {
+        doubleSolenoid.set(state.getValue());
+        pivotstate = state;
+    }
+
+    public CargoPivotState getDoubleSolenoidState(){
+        return pivotstate;
+    }
+
+    public static CargoPivot getInstance() {
+        if (instance == null) {
+            instance = new CargoPivot();
+        }
+        return instance;
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
