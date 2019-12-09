6911c96257abcd6f78ac429f13d5b6ed803f397f
shan <schen51103@gmail.com>
2019-12-09 17:07:54 -0500

    wowowowowowoow finished usecargopivot?????????????????????????????????

--- src/main/java/frc/robot/CargoPivotState.java
+++ src/main/java/frc/robot/CargoPivotState.java
@@ -3,6 +3,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public enum CargoPivotState {
+
     UP(DoubleSolenoid.Value.kForward), DOWN(DoubleSolenoid.Value.kReverse), NEUTRAL(DoubleSolenoid.Value.kOff);
 
     private DoubleSolenoid.Value value;
@@ -14,4 +15,5 @@ public enum CargoPivotState {
     public DoubleSolenoid.Value getValue(){
         return value;
     }
+
 }
--- src/main/java/frc/robot/UseCargoPivot.java
+++ src/main/java/frc/robot/UseCargoPivot.java
@@ -1,5 +1,22 @@
 package frc.robot;
 
-public class UseCargoPivot {
+import edu.wpi.first.wpilibj.command.Command;
 
+public class UseCargoPivot extends Command {
+
+    public UseCargoPivot (){
+        requires(CargoPivot.getInstance());
+    }
+
+    public void initialize(){
+        if(CargoPivot.getInstance().getDoubleSolenoidValue() == CargoPivotState.UP){
+            CargoPivot.getInstance().setDoubleSolenoidValue(CargoPivotState.DOWN);
+        }else{
+            CargoPivot.getInstance().setDoubleSolenoidValue(CargoPivotState.DOWN);
+        }
+    }
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
 }
