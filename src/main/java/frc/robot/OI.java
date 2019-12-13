e4f1b692980a9c450fcf8aaf8db509e39e57d9bb
Navin <7118018@philasd.org>
2019-12-13 17:18:30 -0500

    Fixed Xbox controller

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,8 +3,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
-    private static XboxController xboxController = new XboxController(0);
+    private static XboxController xboxController;
+    public OI() {
+        XboxController xboxController = new XboxController(0);
 
+    }
 
     public static XboxController getXboxController() {
         return xboxController;
